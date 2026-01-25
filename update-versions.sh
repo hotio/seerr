12af070a2dae0e3aab582c0ef67c210f1e48965e
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-25 03:35:48 +0100

    add auth [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -exuo pipefail
 
-version=$(curl -fsSL "https://api.github.com/repos/seerr-team/seerr/commits/develop" | jq -re .sha)
+version=$(curl -fsSL --header "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/seerr-team/seerr/commits/develop" | jq -re .sha)
 json=$(cat meta.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
